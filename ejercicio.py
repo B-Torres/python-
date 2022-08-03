01ff75437ef863513ce180049a1fdc3594532576
B.Torres <braiantorres350@gmail.com>
2022-08-03 11:32:08 -0300

    ejercicio 1

---
+++ ejercicio.py
@@ -0,0 +1,12 @@
+from ast import Num
+from asyncore import read
+from math import dist
+from multiprocessing.sharedctypes import Value
+from turtle import distance
+
+input("ingresar la velocidad y el tiempo de la unidad movil")
+
+v = ENTERO = int( input("velocidad:"))
+t = ENTERO = int( input("tiempo:"))
+d = v*t
+print ("la distancia recorrida es:" , d )
